399661067d6dfe3311e81d6aa19ccd9cb427c20b
Dubrovsky <erek54@ya.ru>
2023-04-14 19:48:29 +0300

    fixed timer

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './timer.css';
 
 const STATUS = {
-  STARTED: false,
+  STARTED: true,
 };
 
 function Timer({ timerMin = 0, timerSec = 0 }) {
@@ -18,12 +18,11 @@ function Timer({ timerMin = 0, timerSec = 0 }) {
           setSeconds(seconds - 1);
         }
         if (seconds === 0) {
-          if (minutes === 0) {
-            clearInterval(interval);
-          } else {
+          if (minutes > 0) {
             setMinutes(minutes - 1);
             setSeconds(59);
           }
+          clearInterval(interval);
         }
       }, 1000);
     }
@@ -40,15 +39,13 @@ function Timer({ timerMin = 0, timerSec = 0 }) {
 
   return (
     <span>
-      {minutes === 0 && seconds === 0 ? null : (
-        <div>
-          <button className="icon icon-play" type="button" aria-label="Start timer" onClick={handleStart} />
-          <button className="icon icon-pause" type="button" aria-label="Pause timer" onClick={handlePause} />
-          <span className="timer">
-            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-          </span>
-        </div>
-      )}
+      <div>
+        <button className="icon icon-play" type="button" aria-label="Start timer" onClick={handleStart} />
+        <button className="icon icon-pause" type="button" aria-label="Pause timer" onClick={handlePause} />
+        <span className="timer">
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </span>
+      </div>
     </span>
   );
 }
