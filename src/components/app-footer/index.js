f6606f35743faf987d1ac085e13a368bef36dd4d
Dubrovsky <erek54@ya.ru>
2023-04-25 22:51:00 +0300

    fixed a structure of components

--- src/components/app-footer/index.js
+++ src/components/app-footer/index.js
@@ -1,3 +1,40 @@
-import AppFooter from './app-footer';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import TasksFilter from '../tasks-filters';
+
+import './app-footer.css';
+
+function AppFooter({
+  itemsLeft,
+  onClearCompleted,
+  onSelectedCompletedFilter,
+  onSelectedActiveFilter,
+  onSelectedAllFilter,
+}) {
+  return (
+    <div className="footer">
+      <span className="todo-count">{itemsLeft} items left</span>
+      <TasksFilter
+        onSelectedCompletedFilter={onSelectedCompletedFilter}
+        onSelectedActiveFilter={onSelectedActiveFilter}
+        onSelectedAllFilter={onSelectedAllFilter}
+      />
+      <button type="button" className="clear-completed" onClick={onClearCompleted}>
+        Clear completed
+      </button>
+    </div>
+  );
+}
+
+AppFooter.defaultProps = {
+  itemsLeft: 0,
+  onClearCompleted: () => {},
+};
+
+AppFooter.propTypes = {
+  itemsLeft: PropTypes.number,
+  onClearCompleted: PropTypes.func,
+};
 
 export default AppFooter;
--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,3 +1,58 @@
-import Timer from './timer';
+import React, { useState, useEffect } from 'react';
+import './timer.css';
+
+const STATUS = {
+  STARTED: false,
+};
+
+function Timer({ timerMin = 0, timerSec = 0 }) {
+  const [minutes, setMinutes] = useState(timerMin);
+  const [seconds, setSeconds] = useState(timerSec);
+  const [status, setStatus] = useState(STATUS.STARTED);
+  let interval;
+  const url =
+    'https://audio-previews.elements.envatousercontent.com/files/188106830/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22LJCSRVX-positive-alert.mp3%22';
+  const audio = new Audio(url);
+
+  useEffect(() => {
+    if (status) {
+      interval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+        }
+        if (seconds === 0) {
+          if (minutes === 0) {
+            audio.play();
+            clearInterval(interval);
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [minutes, seconds, status]);
+
+  const handleStart = () => {
+    setStatus(true);
+  };
+
+  const handlePause = () => {
+    setStatus(false);
+  };
+
+  return (
+    <span>
+      <div>
+        <button className="icon icon-play" type="button" aria-label="Start timer" onClick={handleStart} />
+        <button className="icon icon-pause" type="button" aria-label="Pause timer" onClick={handlePause} />
+        <span className="timer">
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </span>
+      </div>
+    </span>
+  );
+}
 
 export default Timer;
